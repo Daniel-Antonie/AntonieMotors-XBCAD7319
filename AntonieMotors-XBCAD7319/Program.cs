fc00df802f348d205ffbad3d157140d9efd41bb8
Daniel <danielantonie03@gmail.com>
2024-10-16 20:31:29 +0200

    Changed the nav bar , need to add new images and info

--- AntonieMotors-XBCAD7319/Program.cs
+++ AntonieMotors-XBCAD7319/Program.cs
@@ -28,7 +28,7 @@ namespace AntonieMotors_XBCAD7319
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Register}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
