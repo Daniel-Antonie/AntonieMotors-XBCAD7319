5de3bc736b7fa5cebd24b282c9d0c3d762e474d5
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-11-07 17:00:06 +0200

    Admin Ui added.

--- AntonieMotors-XBCAD7319/Controllers/CustomerController.cs
+++ AntonieMotors-XBCAD7319/Controllers/CustomerController.cs
@@ -56,7 +56,8 @@ namespace AntonieMotors_XBCAD7319.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(CustomerModel customer, string confirmPassword)
         {
-            customer.BusinessID = "28bc9a9b52a24d5b9579b5b48a75f4fc"; // Hardcoded business ID
+          //  customer.BusinessID = "28bc9a9b52a24d5b9579b5b48a75f4fc"; // Hardcoded business ID
+            customer.BusinessID = "33a48a2ae69d46b4a4256c3811f8e57c"; // Perlas one 
 
             // 1. Pre-validation: Clear errors and validate the initial fields except CustomerID
             ModelState.Clear();
---
+++ AntonieMotors-XBCAD7319/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AntonieMotors_XBCAD7319.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerDashboard()
+        {
+            return View();
+        }
+    }
+}
